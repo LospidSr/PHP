34fae83f0ba90811c6d590155104fe199e9185ee
LospidSr <sebasrondon17@gmail.com>
2021-10-18 16:04:37 -0300

    cambios

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+	<title>registros</title>
+	<script src="https://kit.fontawesome.com/412f384ddd.js" crossorigin="anonymous"></script>
+</head>
+
+
